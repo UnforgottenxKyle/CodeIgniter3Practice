d6b78a35ad4545b3fabe54c7070051bb72c4d86f
Grifin Kyle Asa <grifin4kyle@gmail.com>
2024-09-13 22:02:05 +0800

    fix the config & autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -39,7 +39,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
 |
 */
-$autoload['packages'] = array('form_validation', 'session');
+$autoload['packages'] = array();
 
 /*
 | -------------------------------------------------------------------
@@ -58,7 +58,7 @@ $autoload['packages'] = array('form_validation', 'session');
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('form_validation', 'session', 'pagination');
 
 /*
 | -------------------------------------------------------------------
